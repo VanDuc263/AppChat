f513d6fb6ae0c36203101c1f64eecf40ed3aa14b
VanDuc <22130050@st.hcmuaf.edu.vn>
2025-12-11 12:27:26 +0700

    revert jsx to tsx

---
+++ src/hooks/useAuthSocketListener.ts
@@ -0,0 +1,40 @@
+import { useEffect } from "react";
+import { getSocket } from "../services/socket";
+import { useAuth } from "../contexts/AuthContext";
+
+interface LoginResponse {
+    event: string;
+    status: "success" | "error";
+    data: {
+        user: string;
+        [key: string]: any;
+    };
+}
+
+export function useAuthSocketListener() {
+    const socket: WebSocket | null = getSocket();
+    const {  user,setUser } = useAuth();
+
+    useEffect(() => {
+        if (!socket) return;
+
+        socket.onmessage = (ev: MessageEvent<string>) => {
+            try {
+                const res: LoginResponse = JSON.parse(ev.data);
+
+                console.log(res);
+
+                if (res.event === "LOGIN" && res.status === "success") {
+
+                }
+            } catch (e) {
+                console.error("Invalid JSON from WebSocket:", e);
+            }
+        };
+
+        // cleanup
+        return () => {
+            socket.onmessage = null;
+        };
+    }, [socket]);
+}
